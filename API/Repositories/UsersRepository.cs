9b3ae5174b9489dbf88f2c917dc02da588e4bdc1
Koushik Ghosh <koushik.ghosh@selise.ch>
2022-04-07 00:26:00 -0700

    Create API for Users

---
+++ API/Repositories/UsersRepository.cs
@@ -0,0 +1,26 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repositories
+{
+    public class UsersRepository: IUsersRepository
+    {
+        private readonly DataContext _dataDbContext;
+        public UsersRepository(DataContext dataDbContext)
+        {
+            _dataDbContext = dataDbContext;
+        }
+
+        public async  Task<List<AppUser>> GetAllUsersAsync()
+        {
+            List<AppUser> users = null;
+            users =  await _dataDbContext.Users.ToListAsync();
+            return users;
+        }
+    }
+}
+public interface IUsersRepository
+{
+    Task <List<AppUser>> GetAllUsersAsync();
+}
---
+++ API/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUsersRepository _usersRepository;
+
+        public UsersController(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetUsersAsync()
+        {
+            var users = await _usersRepository.GetAllUsersAsync();
+            return Ok(users);
+        }
+    }
+}

b4a3f7d009fd31c4490466e04eddbad4756e61a6
Koushik Ghosh <koushik.ghosh@selise.ch>
2022-04-11 01:18:31 -0700

    Add angular client

--- API/Program.cs
+++ API/Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,9 +29,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
